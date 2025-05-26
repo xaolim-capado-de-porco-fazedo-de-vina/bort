35ffdc9facecd802929db220a35d25ea31227fdc
xaolim-capado-de-porco-fazedo-de-vina <bortolozzo.joao@escola.pr.gov.br>
2025-05-26 18:30:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'Gremio?',
+    '  é um clube de futebol profissional brasileiro sediado em Porto Alegre, no Rio Grande do Sul. Fundado em 15 de setembro de 1903, é considerado um dos maiores e mais tradicionais clubes do Brasil e da América do Sul'
+)
+
+criaCartao(
+    'futebol',
+    'Titulos?',
+    'O Gremio tem 94 titulos oficiais'
+)
+
+criaCartao(
+    'futebol',
+    'Maior Rival?',
+    'é o Internacional.'
+)
+
+criaCartao(
+    'futebol',
+    'Maior jogador da historia do clube?',
+    'Renato Gaúcho)'
+)
